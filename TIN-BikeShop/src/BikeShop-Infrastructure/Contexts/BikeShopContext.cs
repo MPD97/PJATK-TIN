48932c0faa5f73b0ae4b061f29a6ec290cf5e44f
MPD97 <mateuszpracadom@gmail.com>
2020-10-18 15:18:28 +0200

    DbSets

--- TIN-BikeShop/src/BikeShop-Infrastructure/Contexts/BikeShopContext.cs
+++ TIN-BikeShop/src/BikeShop-Infrastructure/Contexts/BikeShopContext.cs
@@ -9,12 +9,28 @@ namespace BikeShop_Infrastructure.Contexts
 {
     public class BikeShopContext : IdentityDbContext<ApplicationUser, ApplicationRole, int>
     {
+        public DbSet<Address> Addresses { get; set; }
+        public DbSet<Language> Languages { get; set; }
+        public DbSet<Product> Products { get; set; }
+        public DbSet<ProductDescriptionTranslation> ProductDescriptions { get; set; }
+        public DbSet<ProductNameTranslation> ProductNames { get; set; }
+        public DbSet<Shop> Shops { get; set; }
+        public DbSet<Storage> Storages { get; set; }
+
         public BikeShopContext(DbContextOptions options) : base(options)
         {
         }
 
         protected BikeShopContext()
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder builder)
+        {
+            base.OnModelCreating(builder);
+
+
+
         }
     }
 

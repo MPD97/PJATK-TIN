60c299d031ade77bd2c76a3069acb72da12d35f7
MPD97 <mateuszpracadom@gmail.com>
2020-10-18 14:54:40 +0200

    Creating identity entities

---
+++ TIN-BikeShop/src/BikeShop-Core/Entities/ApplicationUser.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Identity;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeShop_Core.Entities
+{
+    public class ApplicationUser : IdentityUser
+    {
+
+    }
+    public class AppClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
+    {
+        public AppClaimsPrincipalFactory(
+            UserManager<ApplicationUser> userManager
+            , RoleManager<IdentityRole> roleManager
+            , IOptions<IdentityOptions> optionsAccessor)
+        : base(userManager, roleManager, optionsAccessor)
+        { }
+    }
+}
+

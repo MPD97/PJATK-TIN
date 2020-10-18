60a33bad9db217b7748a7cb840383ba27b8b2c2f
MPD97 <mateuszpracadom@gmail.com>
2020-10-18 15:07:17 +0200

    Identity entities with role

--- TIN-BikeShop/src/BikeShop-Core/Entities/ApplicationUser.cs
+++ TIN-BikeShop/src/BikeShop-Core/Entities/ApplicationUser.cs
@@ -1,22 +1,24 @@
 ﻿using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Options;
 using System;
 using System.Collections.Generic;
+using System.Security.Claims;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace BikeShop_Core.Entities
 {
-    public class ApplicationUser : IdentityUser
+    public class ApplicationUser : IdentityUser<int>
     {
 
     }
-    public class AppClaimsPrincipalFactory : UserClaimsPrincipalFactory<ApplicationUser, IdentityRole>
+    public class AppRole : IdentityRole<int>
     {
-        public AppClaimsPrincipalFactory(
-            UserManager<ApplicationUser> userManager
-            , RoleManager<IdentityRole> roleManager
-            , IOptions<IdentityOptions> optionsAccessor)
-        : base(userManager, roleManager, optionsAccessor)
-        { }
+        public AppRole() { }
+        public AppRole(string name)
+        {
+            Name = name;
+        }
     }
 }
 

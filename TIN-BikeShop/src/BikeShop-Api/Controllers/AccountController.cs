4e232715636496f4809d644be30209d9bb39ab65
MPD97 <mateuszpracadom@gmail.com>
2020-10-19 21:32:29 +0200

    JWT Token 2/3

--- TIN-BikeShop/src/BikeShop-Api/Controllers/AccountController.cs
+++ TIN-BikeShop/src/BikeShop-Api/Controllers/AccountController.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
 using System.Linq;
+using System.Security.Claims;
 using System.Text;
 using System.Threading.Tasks;
 using BikeShop_Core.Entities;
@@ -10,6 +12,7 @@ using BikeShop_Infrastructure.Contexts;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.JsonWebTokens;
 using Microsoft.IdentityModel.Tokens;
 
 namespace BikeShop_Api.Controllers
@@ -44,6 +47,31 @@ namespace BikeShop_Api.Controllers
             }
 
             var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(BikeShopJwtConstrains.Key));
+            var creds = new SigningCredentials(key, SecurityAlgorithms.HmacSha256);
+
+            var claims = new[]
+            {
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Sub, model.UserName),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.Jti, Guid.NewGuid().ToString()),
+                new Claim(System.IdentityModel.Tokens.Jwt.JwtRegisteredClaimNames.UniqueName, model.UserName),
+            };
+
+            var token = new JwtSecurityToken
+            (
+                BikeShopJwtConstrains.Issuer,
+                BikeShopJwtConstrains.Audience,
+                claims,
+                expires: DateTime.UtcNow.AddDays(1),
+                signingCredentials: creds
+            );
+
+            var results = new
+            {
+                token = new JwtSecurityTokenHandler().WriteToken(token),
+                expiration = token.ValidTo
+            };
+
+            return Created("", results);
         }
     }
 }

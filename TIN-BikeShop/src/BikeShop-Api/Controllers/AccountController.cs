6246895d2fa5f4cbbb1870015d68d997d34517d8
MPD97 <mateuszpracadom@gmail.com>
2020-10-19 21:25:36 +0200

    JWT Token 1/3

--- TIN-BikeShop/src/BikeShop-Api/Controllers/AccountController.cs
+++ TIN-BikeShop/src/BikeShop-Api/Controllers/AccountController.cs
@@ -1,9 +1,16 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Text;
 using System.Threading.Tasks;
+using BikeShop_Core.Entities;
+using BikeShop_Infrastructure.Authorization;
+using BikeShop_Infrastructure.Authorization.Models;
+using BikeShop_Infrastructure.Contexts;
 using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.AspNetCore.Mvc;
+using Microsoft.IdentityModel.Tokens;
 
 namespace BikeShop_Api.Controllers
 {
@@ -11,5 +18,32 @@ namespace BikeShop_Api.Controllers
     [ApiController]
     public class AccountController : ControllerBase
     {
+        private readonly UserManager<ApplicationUser> _userManager;
+        private readonly SignInManager<ApplicationUser> _signInManager;
+        private readonly BikeShopContext _context;
+
+        public AccountController(UserManager<ApplicationUser> userManager, SignInManager<ApplicationUser> signInManager, BikeShopContext context)
+        {
+            _userManager = userManager;
+            _signInManager = signInManager;
+            _context = context;
+        }
+
+        public async Task<IActionResult> CreateToken([FromBody] JwtTokenModel model)
+        {
+            if (!ModelState.IsValid)
+            {
+                return BadRequest();
+            }
+            ApplicationUser user = await _userManager.FindByNameAsync(model.UserName);
+
+            var signInResult = await _signInManager.CheckPasswordSignInAsync(user, model.Password, false);
+            if (!signInResult.Succeeded)
+            {
+                return BadRequest();
+            }
+
+            var key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(BikeShopJwtConstrains.Key));
+        }
     }
 }
---
+++ TIN-BikeShop/src/BikeShop-Infrastructure/Authorization/BikeShopJwtConstrains.cs
@@ -0,0 +1,13 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeShop_Infrastructure.Authorization
+{
+    public class BikeShopJwtConstrains
+    {
+        public const string Issuer = "BikeShop";
+        public const string Audience = "ApplicationUser";
+        public const string Key = "12345654323456";
+    }
+}

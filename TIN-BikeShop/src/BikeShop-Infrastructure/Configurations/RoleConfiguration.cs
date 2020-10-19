febb02aefdc80b9e9c9bd13bafe17aa980351e53
MPD97 <mateuszpracadom@gmail.com>
2020-10-19 20:23:16 +0200

    Role

---
+++ TIN-BikeShop/src/BikeShop-Infrastructure/Configurations/RoleConfiguration.cs
@@ -0,0 +1,17 @@
+﻿using BikeShop_Core.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeShop_Infrastructure.Configurations
+{
+    public class RoleConfiguration : IEntityTypeConfiguration<ApplicationRole>
+    {
+        public void Configure(EntityTypeBuilder<ApplicationRole> builder)
+        {
+           
+        }
+    }
+}

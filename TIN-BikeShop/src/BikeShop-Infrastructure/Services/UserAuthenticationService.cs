25fb2098021c751c5b1abd10ad358bd8f1f3bb35
MPD97 <mateuszpracadom@gmail.com>
2020-10-22 20:43:21 +0200

    Creating better service for authorization

---
+++ TIN-BikeShop/src/BikeShop-Infrastructure/Services/UserAuthenticationService.cs
@@ -0,0 +1,31 @@
+﻿using BikeShop_Core.Entities;
+using BikeShop_Infrastructure.Contexts;
+using Microsoft.AspNetCore.Http;
+using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeShop_Infrastructure.Services
+{
+    public class UserAuthenticationService
+    {
+        private readonly BikeShopContext _context;
+        private readonly IHttpContextAccessor _httpAccessor;
+        private readonly UserAuthorizationSettings _settings;
+
+        public UserAuthenticationService(BikeShopContext context, IHttpContextAccessor httpAccessor, UserAuthorizationSettings settings)
+        {
+            _context = context;
+            _httpAccessor = httpAccessor;
+            _settings = settings;
+        }
+
+        public string GenerateJwtToken(ApplicationUser user)
+        {
+            throw new NotImplementedException(); 
+        }
+
+
+
+    }
+}
---
+++ TIN-BikeShop/src/BikeShop-Infrastructure/Services/UserAuthorizationSettings.cs
@@ -0,0 +1,10 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace BikeShop_Infrastructure.Services
+{
+    public class UserAuthorizationSettings
+    {
+    }
+}

c0eea59b97e2e88c3f853e1312514f33e174cb07
MPD97 <mateuszpracadom@gmail.com>
2020-10-24 17:13:33 +0200

    Get

--- TIN-BikeShop/src/BikeShop-Infrastructure/Services/Shop/ShopService.cs
+++ TIN-BikeShop/src/BikeShop-Infrastructure/Services/Shop/ShopService.cs
@@ -1,6 +1,9 @@
-﻿using BikeShop_Infrastructure.Services.Shop.Models;
+﻿using BikeShop_Infrastructure.Contexts;
+using BikeShop_Infrastructure.Services.Shop.Models;
+using Microsoft.EntityFrameworkCore;
 using System;
 using System.Collections.Generic;
+using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -8,9 +11,29 @@ namespace BikeShop_Infrastructure.Services.Shop
 {
     public class ShopService
     {
-        public Task<ShopResponseModel> Get(byte id)
-        {
+        private readonly BikeShopContext _context;
 
+        public ShopService(BikeShopContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<ShopResponseModel> Get(byte id)
+        {
+            var res = await _context.Shops
+                .Where(shop => shop.ShopId == id)
+                .Include(shop => shop.Address)
+                .Select(shop => new ShopResponseModel
+                {
+                    ShopId = shop.ShopId,
+                    Name = shop.Name,
+                    PhotoPath = shop.PhotoPath,
+                    City = shop.Address.City,
+                    Street = shop.Address.Street,
+                    StreetNumber = shop.Address.StreetNumber,
+                    ZipCode = shop.Address.ZipCode
+                }).FirstOrDefaultAsync();
+            return res;
         }
     }
 }

eb5f32aff5476646e18d2fa30b4dedc6f5a32a69
MPD97 <mateuszpracadom@gmail.com>
2020-10-24 17:08:14 +0200

    ShopService

---
+++ TIN-BikeShop/src/BikeShop-Infrastructure/Services/Shop/ShopService.cs
@@ -0,0 +1,16 @@
+﻿using BikeShop_Infrastructure.Services.Shop.Models;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BikeShop_Infrastructure.Services.Shop
+{
+    public class ShopService
+    {
+        public Task<ShopResponseModel> Get(byte id)
+        {
+
+        }
+    }
+}

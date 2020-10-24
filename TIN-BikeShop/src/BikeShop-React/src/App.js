eb9e39a3f1460e9f9561bf88bbe2291c5686f647
MPD97 <mateuszpracadom@gmail.com>
2020-10-24 14:46:57 +0200

    Creating navbar

--- TIN-BikeShop/src/BikeShop-React/src/App.js
+++ TIN-BikeShop/src/BikeShop-React/src/App.js
@@ -5,7 +5,26 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
+        <div className="Header-Nav">
+          <div className="Header-Nav__Link">
+            Strona Główna 1
+          </div>
+          <div className="Header-Nav__Link">
+            Strona Główna 2
+          </div>
+        </div>
+        <div className="Header-Nav">
+          <div className="Header-Nav__Link">
+            Logowanie
+        </div>
+          <div className="Header-Nav__Link">
+            Wyloguj
+          </div>
+        </div>
       </header>
+      <div className="App-Content">
+        This is content
+      </div>
     </div>
   );
 }

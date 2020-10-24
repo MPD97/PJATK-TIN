58c3d67ecf23a93f429b7c12dbb4c5295a3af92a
MPD97 <mateuszpracadom@gmail.com>
2020-10-24 16:24:34 +0200

    Login HTML

--- TIN-BikeShop/src/BikeShop-React/src/LogIn/LogIn.js
+++ TIN-BikeShop/src/BikeShop-React/src/LogIn/LogIn.js
@@ -2,11 +2,30 @@ import React from 'react';
 import './LogIn.css';
 
 function LogIn() {
-  return (
-    <div className="LogIn">
-        Login
-    </div>
-  );
+    return (
+        <div className="LogIn">
+            <div className="LogIn-Container">
+                <div className="LogIn-Header">
+                    <h1>Logowanie</h1>
+                    <hr />
+                </div>
+                <div className="LogIn-Form">
+                    <div className="LogIn-Form__Element-Container">
+                        <label for="UserName">Nazwa Użytkownika: </label>
+                        <input type="text" name="UserName" required />
+                    </div>
+                    <div className="LogIn-Form__Element-Container">
+                        <label for="Password">Hasło:</label>
+                        <input type="password" name="Password" required />
+                    </div>
+                    <div className="LogIn-Form__Element-Container-Submit">
+                        <button type="submit">Zaloguj</button>
+                    </div>
+                </div>
+
+            </div>
+        </div>
+    );
 }
 
 export default LogIn;
